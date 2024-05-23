63be6c2c014bff0d8f5101563c18a85bbbf21b39
Arthur Gobin <arthurgobin72@gmail.com>
2024-05-23 15:40:37 +0200

    simple version without db

--- app/controllers/FlightController.scala
+++ app/controllers/FlightController.scala
@@ -1,34 +1,64 @@
 package controllers
 
-import models.Flight
+import models.{Airport, Flight, Plane}
 
 import javax.inject._
 import play.api.mvc._
+import play.api.mvc
 import services.FlightService
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
-class FlightController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class FlightController @Inject()(val controllerComponents: ControllerComponents, flightService: FlightService) extends BaseController {
 
-  private val flightService = new FlightService()
+  def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val flights = flightService.getFlights
+    Ok(views.html.flights(flights))
+  }
 
+  def delete(id: Long): Action[AnyContent] = Action {
+    flightService.deleteFlight(id)
+    Redirect(routes.FlightController.index)
+  }
 
+  def addFlight: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.addFlight(flightService.getFlights, flightService.getAirports, flightService.getPlanes))
+  }
 
-  val flights: List[Flight] = flightService.getFlights()
+  def create: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    implicit val ec: ExecutionContext = ExecutionContext.global
 
+    println("test " + request.body.asFormUrlEncoded.get("departureTime"))
+    val departureAirportId = request.body.asFormUrlEncoded.get("departureAirport").head.toLong
+    // to display info in the logs :
+    println("departureAirportId: " + departureAirportId)
+    val arrivalAirportId = request.body.asFormUrlEncoded.get("arrivalAirport").head.toLong
+    val departureTime = request.body.asFormUrlEncoded.get("departureTime").head
+    val arrivalTime = request.body.asFormUrlEncoded.get("arrivalTime").head
+    val planeId = request.body.asFormUrlEncoded.get("plane").head.toLong
 
-    /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-    def index() = Action { implicit request: Request[AnyContent] =>
-      Ok(views.html.flights(flights))
+      val airports = flightService.getAirports
+      val planes = flightService.getPlanes
+      val flights = flightService.getFlights
+      val newFlight = Flight(
+        id = flights.size + 1,
+        departureAirport = airports.find(_.id == departureAirportId).getOrElse(Airport(0, "", "", "", "")),
+        arrivalAirport = airports.find(_.id == arrivalAirportId).getOrElse(Airport(0, "", "", "", "")),
+        departureTime = LocalDateTime.parse(departureTime, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")),
+        arrivalTime = LocalDateTime.parse(arrivalTime, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")),
+        plane = planes.find(_.id == planeId).getOrElse(Plane(0, "", 0)),
+        status = "Scheduled"
+      )
+
+      flightService.addFlight(newFlight).map { _ =>
+        Redirect(routes.FlightController.index)
+      }
     }
+
+
+
 
 }
--- app/services/FlightService.scala
+++ app/services/FlightService.scala
@@ -4,91 +4,72 @@ import models.Flight
 import models.Airport
 import models.Plane
 
-
 import java.time.LocalDateTime
-//import play.api.db.Database
-//import anorm._
-//import anorm.SqlParser.get
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-
-/*class FlightService(db: Database) {
-
-  private val simple = {
-    get[Long]("flight.id") ~
-      get[Long]("flight.departure_airport_id") ~
-      get[Long]("flight.arrival_airport_id") ~
-      get[LocalDateTime]("flight.departure_time") ~
-      get[LocalDateTime]("flight.arrival_time") ~
-      get[Long]("flight.plane_id") ~
-      get[String]("flight.status") map {
-      case id ~ departureAirportId ~ arrivalAirportId ~ departureTime ~ arrivalTime ~ planeId ~ status => Flight(id, Airport(departureAirportId), Airport(arrivalAirportId), departureTime, arrivalTime, PlaneService.getPlaneById(planeId).get, status)
-    }
-  }
-
-  def options: Seq[(String, String)] = db.withConnection { implicit connection =>
-    SQL("select * from flight order by departure_time desc").as(simple *).map(c => c.id.toString -> c.departureTime.toString)
-  }
-
-  def findById(id: Long): Option[Flight] = {
-    db.withConnection { implicit connection =>
-      SQL(
-        """
-          select * from flight
-          where id = {id}
-        """
-      ).on(
-        "id" -> id
-      ).as(simple.singleOpt)
-    }
-  }
-
-  def findByDepartureAirport(departureAirport: Long): Seq[Flight] = {
-    db.withConnection { implicit connection =>
-      SQL(
-        """
-          select * from flight
-          where departure_airport_id = {departureAirport}
-          order by departure_time
-        """
-      ).on(
-        "departureAirport" -> departureAirport
-      ).as(simple *)
-    }
-  }
-
-  def findByArrivalAirport(arrivalAirport: Long): Seq[Flight] = {
-    db.withConnection { implicit connection =>
-      SQL(
-        """
-          select * from flight
-          where arrival_airport_id = {arrivalAirport}
-          order by arrival_time
-        """
-      ).on(
-        "arrivalAirport" -> arrivalAirport
-      ).as(simple *)
-    }
-  }
-
-  def insert(flight: Flight): Unit = {
-    db.withConnection { implicit connection =>
-      val sql = SQL("insert into flight(departure_airport_id, arrival_airport_id, departure_time, arrival_time, plane_id, status) values({departureAirportId}, {arrivalAirportId}, {departureTime
-
-
-
- */
 
 class FlightService {
 
-    private val flights = List(
-      Flight(1, Airport(1, "John F. Kennedy International Airport", "JFK", "New York", "USA"), Airport(2, "Los Angeles International Airport", "LAX", "Los Angeles", "USA"), LocalDateTime.of(2018, 1, 1, 10, 0), LocalDateTime.of(2018, 1, 1, 13, 0), Plane(1, "Boeing 747", 400), "Active"),
-      Flight(2, Airport(2, "Los Angeles International Airport", "LAX", "Los Angeles", "USA"), Airport(3, "Heathrow Airport", "LHR", "London", "UK"), LocalDateTime.of(2018, 1, 2, 10, 0), LocalDateTime.of(2018, 1, 2, 13, 0), Plane(2, "Airbus A380", 600), "Active"),
-      Flight(3, Airport(3, "Heathrow Airport", "LHR", "London", "UK"), Airport(1, "John F. Kennedy International Airport", "JFK", "New York", "USA"), LocalDateTime.of(2018, 1, 3, 10, 0), LocalDateTime.of(2018, 1, 3, 13, 0), Plane(3, "Boeing 737", 200), "Active")
+  private val planes = List(
+      Plane(1, "Boeing 747", 400),
+      Plane(2, "Airbus A380", 600),
+      Plane(3, "Boeing 737", 200)
     )
 
-    def getFlights(): List[Flight] = {
+  private val airports = List(
+      Airport(1, "Los Angeles International Airport", "LAX", "Los Angeles", "USA"),
+      Airport(2, "JFK International Airport", "JFK", "New York", "USA"),
+      Airport(3, "London Heathrow", "LHR", "London", "UK"),
+      Airport(4, "Schiphol", "AMS", "Amsterdam", "Netherlands"),
+      Airport(5, "Charles de Gaulle", "CDG", "Paris", "France"),
+      Airport(6, "Frankfurt Airport", "FRA", "Frankfurt", "Germany")
+    )
+
+  private var flights = List(
+      Flight(1, airports(0), airports(1), LocalDateTime.of(2021, 8, 1, 9, 0), LocalDateTime.of(2021, 8, 1, 12, 0), planes(0), "Scheduled"),
+      Flight(2, airports(1), airports(0), LocalDateTime.of(2021, 8, 2, 9, 0), LocalDateTime.of(2021, 8, 2, 12, 0), planes(1), "Scheduled"),
+      Flight(3, airports(2), airports(3), LocalDateTime.of(2021, 8, 3, 9, 0), LocalDateTime.of(2021, 8, 3, 12, 0), planes(2), "Scheduled"),
+      Flight(4, airports(3), airports(2), LocalDateTime.of(2021, 8, 4, 9, 0), LocalDateTime.of(2021, 8, 4, 12, 0), planes(0), "Scheduled"),
+      Flight(5, airports(4), airports(5), LocalDateTime.of(2021, 8, 5, 9, 0), LocalDateTime.of(2021, 8, 5, 12, 0), planes(1), "Scheduled"),
+      Flight(6, airports(5), airports(4), LocalDateTime.of(2021, 8, 6, 9, 0), LocalDateTime.of(2021, 8, 6, 12, 0), planes(2), "Scheduled")
+    )
+
+
+
+    def getFlights: List[Flight] = {
       flights
     }
+
+    def getAirports: List[Airport] = {
+      airports
+    }
+
+    def getPlanes: List[Plane] = {
+      planes
+    }
+
+    def getFlightById(id: Long): Option[Flight] = {
+      flights.find(_.id == id)
+    }
+
+  def getAirportById(id: Long): Option[Airport] = {
+    airports.find(_.id == id)
+  }
+
+  def getPlaneById(id: Long): Option[Plane] = {
+    planes.find(_.id == id)
+  }
+
+  def deleteFlight(id: Long): Unit = {
+    flights = flights.filterNot(_.id == id)
+  }
+
+
+  def addFlight(flight: Flight): Future[Unit] = Future {
+    flights = flights :+ flight
+  }
+
 
 
 }
